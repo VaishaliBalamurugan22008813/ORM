4bc8204fe86aa6bd4e3fec1738270a77aa11f995
VaishaliBalamurugan22008813 <vaishalibalamurugan317@gmail.com>
2023-01-13 19:34:39 +0000

    Success

--- dataproject/dataproject/settings.py
+++ dataproject/dataproject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'myapp',
 ]
 
 ROOT_URLCONF = 'dataproject.urls'
